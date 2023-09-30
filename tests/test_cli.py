75ed0cdfb4da42c73666c2c0d411dbba2a585676
John Vivian <jtvivian@gmail.com>
2023-09-30 01:24:09 -0700

    Update tests / coverage

---
+++ tests/test_cli.py
@@ -0,0 +1,16 @@
+import os
+from pathlib import Path
+
+from typer.testing import CliRunner
+
+from covid19_drdfm.cli import app
+
+runner = CliRunner()
+
+
+def test_process():
+    res = runner.invoke(app, ["process"])
+    path = Path("./outfile.parquet")
+    assert res.exit_code == 0
+    assert path.exists()
+    os.remove(path)
