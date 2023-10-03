5d0dc3cb968cff0d77106151a82ee63dda3720dc
John Vivian <jtvivian@gmail.com>
2023-10-02 20:28:22 -0700

    Update tests and coverage

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,11 @@
 import os
 from pathlib import Path
 
+import pytest
 from typer.testing import CliRunner
 
-from covid19_drdfm.cli import app
+from covid19_drdfm.cli import PreprocessingFailure, app, process_data
+from covid19_drdfm.processing import get_df
 
 runner = CliRunner()
 
@@ -14,6 +16,8 @@ def test_process():
     assert res.exit_code == 0
     assert path.exists()
     os.remove(path)
+    with pytest.raises(PreprocessingFailure):
+        process_data("/foo/bar/zoobar/file.pq")
 
 
 def test_run():
