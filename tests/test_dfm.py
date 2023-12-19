0591e689792ca7553943ffc78b684414e4a594b3
John Vivian <jtvivian@gmail.com>
2023-12-19 14:49:47 -0800

    Update tests and coverage

--- tests/test_dfm.py
+++ tests/test_dfm.py
@@ -9,7 +9,8 @@ from covid19_drdfm.processing import get_df
 # TODO: output should go in a directory instead of dumping shit everywhere
 def test_run_model():
     df = get_df()
-    run_model(df, "NY", Path("./testdir"))
-    assert Path("./testdir/NY/model.csv").exists()
-    assert Path("./testdir/NY/results.csv").exists()
+    state = "SD"
+    run_model(df, state, Path("./testdir"))
+    assert Path("./testdir/SD/model.csv").exists()
+    assert Path("./testdir/SD/results.csv").exists()
     shutil.rmtree("./testdir")
