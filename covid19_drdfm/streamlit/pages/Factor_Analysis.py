9cf564443535b9540cc2ea5000df2ca2d65e9174
John Vivian <jtvivian@gmail.com>
2024-02-04 22:32:19 -0800

    clean up imports and update cov

--- covid19_drdfm/streamlit/pages/Factor_Analysis.py
+++ covid19_drdfm/streamlit/pages/Factor_Analysis.py
@@ -1,21 +1,12 @@
-import streamlit as st
-import json
-import time
-from collections import defaultdict
 from pathlib import Path
 
 import pandas as pd
 import plotly.io as pio
 import plotly_express as px
-import statsmodels.api as sm
 import streamlit as st
-from rich import print as pprint
-from sklearn.preprocessing import MinMaxScaler
 
 from covid19_drdfm.constants import FACTORS_GROUPED
-from covid19_drdfm.dfm import state_process
 from covid19_drdfm.processing import get_df, normalize
-
 
 st.set_page_config(layout="wide")
 pio.templates.default = "plotly_white"
