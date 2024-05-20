d6b229e4d39bab2135ad0c9d4ed69ff19283e340
jvivian <jtvivian@gmail.com>
2024-05-19 18:24:46 -0700

    Remove buggy anndata code for now

--- covid19_drdfm/dfm.py
+++ covid19_drdfm/dfm.py
@@ -103,8 +103,8 @@ class ModelRunner:
             filtered_factors = process_factors(res.factors["filtered"], data.raw, batch.obs)
             result = Result(batch_name, res, model, filtered_factors)
             result.write(self.outdir)
-            self.ad.uns["factors"] = result.factors.drop(columns="Time")
-            self.ad.write(self.outdir / batch_name / "data.h5ad")
+            # self.ad.uns["factors"] = result.factors.drop(columns="Time")
+            # self.ad.write(self.outdir / batch_name / "data.h5ad")
             self.results.append(result)
         # TODO: Concat factors across batch variables
         print("All runs completed!")
